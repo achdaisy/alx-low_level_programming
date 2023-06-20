50b81f8fbe25ef6cc4771d32543d5ab8fb3b711a
achdaisy <oduordaizy@gmail.com>
2023-06-20 11:05:45 -0700

    Created 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+void print_alphabet(void)
+{
+char letter;
+for (letter = 'a'; letter <= letter; letter++)
+	_putchar(letter);
+
+}
+_putchar('\n');
+return (0);
+}
