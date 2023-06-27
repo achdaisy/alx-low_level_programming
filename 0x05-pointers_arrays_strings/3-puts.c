29b50a9fb76214f0cc1463454bcb89b3695f17a6
achdaisy <oduordaizy@gmail.com>
2023-06-27 16:06:24 -0700

    Created 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * 3-puts.c - prints a string, followed by a new line, to stdout.
+ * @str: the string to be printed.
+ *
+ * Return: void.
+ */
+void _puts(char *str)
+{
+	int  i = 0;
+
+	for (; *str != '0'; str++)
+	{
+		_putchar(*str);
+	}
+
+	_putchar('\n');
+}
