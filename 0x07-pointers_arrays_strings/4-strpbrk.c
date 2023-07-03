e8f40dfb94a9cf01196f43711159dda96ee5506c
achdaisy <oduordaizy@gmail.com>
2023-07-03 16:55:05 -0700

    Created 4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+  * _strpbrk - search a string for any of a set of bytes
+  * @s: source string
+  * @accept: accepted characters
+  *
+  * Return: returns the first  pointer in s that matches accept
+  */
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0, j;
+
+	while (s[i])
+	{
+		j = 0;
+
+		while (accept[j)
+		{
+			if (s[i] == accept[j])
+			{
+				s += i;
+				return (s);
+			}
+
+			j++;
+		}
+
+		i++;
+	}
+
+	return ('\0');
+}
+
