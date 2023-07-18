683ec8ed60b326194dc9034dc6a4425f094ff9b0
achdaisy <oduordaizy@gmail.com>
2023-07-18 09:32:34 -0700

    Created 4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog
+ * @name: pointer to name of dog
+ * @age: the dog's age
+ * @owner: pointer to dog owner
+ *
+ * Return: 0
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i, j, k;
+	dog_t *dog_1;
+
+	/*calculating length of name string*/
+	for (i = 0 ; name[i] != '\0'; i++)
+		;
+	i++;
+	/*calculating the length of owner*/
+	for (i = 0; owner[j] != '\0'; j++)
+		;
+	j++;
+	dog_1 = malloc(sizeof(dog_t));
+	if (dog_1->name == NULL)
+	{
+		free(dog_1->name);
+		free(dog_1);
+		return (NULL);
+	}
+	/*storing name to the allocated space*/
+	for (k = 0; k < i ; k++)
+		dog_1->name[k] = name[k];
+	dog_1->name[k] = name[k];
+	dog_1->age = age;
+	dog_1->owner = malloc(j * sizeof(dog_t));
+	if (dog_1->owner == NULL)
+	{
+		free(dog_1->name);
+		free(dog_1->owner);
+		free(dog_1);
+		return (NULL);
+	}
+	for (k = 0; k < j ; k++)
+		dog_1->owner[k] = owner[k];
+	dog_1->owner[k] = owner[k];
+	return (dog_1);
+}
