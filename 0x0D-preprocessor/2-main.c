1bd55d9dc7c23b40daf47b2c6aaedc498f4db687
achdaisy <oduordaizy@gmail.com>
2023-07-17 17:56:11 -0700

    fixed betty errors

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-/*
- * main: prints the name of the file a program was compiled from.
+/**
+ *main - prints the name of the file a program was compiled from.
  *
  * Return: 0
  */
 
-int main()
+int main(void)
 {
-	printf("%s \n", __FILE__ );
+	printf("%s \n", __FILE__);
 	return (0);
 }
