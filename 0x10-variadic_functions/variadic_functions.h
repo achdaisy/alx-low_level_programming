d38dadf3f225a89012db24db12a430dec21284ce
achdaisy <oduordaizy@gmail.com>
2023-07-20 15:21:06 -0700

    Edited the header file variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,16 @@
-#ifndef <VARIADIC_FUNCTIONS.H>
-#define <VARIADIC_FUNCTIONS.H>
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
+
+typedef struct my_types
+{
+	char *symbol
+	void (*f)();
+}types_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
 #endif 
