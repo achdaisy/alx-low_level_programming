d3f86c5371848ab50addaf4a52fa6ce476df9613
achdaisy <oduordaizy@gmail.com>
2023-07-19 18:58:28 -0700

    Modified the structure

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,13 +11,11 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (array != NULL || action != NULL)
-	{
-		while (size-- > 0)
-		{
-			action(*array);
-			array++;
-		}
+	if (array == NULL || action == NULL)
 		return;
+	while (size-- > 0)
+	{
+		action(*array);
+		array++;
 	}
 }
