e686d5574800a32531c0921bc324387104100f18
achdaisy <oduordaizy@gmail.com>
2023-07-28 23:26:44 -0700

    Fixed betty errors for variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,11 +2,18 @@
 #define VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
 
+/**
+ * struct my_types - A new struct type defining a printer.
+ * @symbol: A symbol representing a data type.
+ * @f: A function pointer to a function that prints
+ *         a data type corresponding to symbol.
+ */
+
 typedef struct my_types
 {
 	char *symbol;
 	void (*f)();
-}types_t;
+} types_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
@@ -16,4 +23,4 @@ char *make_nil(char *s);
 void print_comma(int j, int x);
 int count_format(const char * const format);
 
-#endif 
+#endif
