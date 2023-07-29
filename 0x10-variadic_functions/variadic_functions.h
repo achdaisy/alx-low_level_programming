d86e57a67ec7f3d1030b5de0d9215129c2a37ede
achdaisy <oduordaizy@gmail.com>
2023-07-28 23:07:59 -0700

    Added more function prototypes

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -12,5 +12,8 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+char *make_nil(char *s);
+void print_comma(int j, int x);
+int count_format(const char * const format);
 
 #endif 
