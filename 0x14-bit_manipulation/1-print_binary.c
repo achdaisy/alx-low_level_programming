cec0ddbca2b085751d6a25a78782315e4db61ec8
achdaisy <oduordaizy@gmail.com>
2023-08-03 15:35:31 -0700

    Added 1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_binary - prints binary representation of a number
+ * @n: the decimal number
+ *
+ * Return: the binary number
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+
+	_putchar((n & 1) + '0');
+}
