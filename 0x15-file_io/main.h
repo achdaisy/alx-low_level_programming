bcda33c9444ff3db68c05b1cfe88e6a3ea7e0564
achdaisy <oduordaizy@gmail.com>
2023-09-04 15:26:59 -0700

    some modifications

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,9 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <string.h>
+#include <fcntl.h>
+
+#define BUFSIZE 1024
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
